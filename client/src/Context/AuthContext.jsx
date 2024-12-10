758c5293f11d49f1863ad14cce50134944f29d5e
sabbir-offc <mdsabbirhowlader420@gmail.com>
2024-12-11 05:13:15 +0600

    removed mongodb credential from github

--- client/src/Context/AuthContext.jsx
+++ client/src/Context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import  { createContext, useContext, useState, useEffect } from "react";
 
 // Create a context for authentication
 const AuthContext = createContext();
